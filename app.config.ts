151748b0fca26c7afa589e4a492ab371f7394e9e
Brent Vatne <brentvatne@gmail.com>
2022-09-23 22:01:24 -0700

    Add dev icon

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: process.env.RELEASE ? config.name : `${config.name} (dev)`,
   slug: process.env.RELEASE ? config.slug : `${config.slug}-dev`,
+  icon: process.env.RELEASE ? config.icon : "./assets/icon-dev.png",
   ios: {
     ...config.ios,
     bundleIdentifier: process.env.RELEASE
