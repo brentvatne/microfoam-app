8ebf246564125e12226e7e9dddbefe2040a086e2
Brent Vatne <brentvatne@gmail.com>
2022-09-30 23:48:08 -0700

    Add some info to settings

--- app/(root)/(tabs)/settings.tsx
+++ app/(root)/(tabs)/settings.tsx
@@ -1,19 +1,22 @@
-import { Alert, Button, View } from "react-native";
+import { ScrollView, Alert, Button, Text, View } from "react-native";
+import * as Application from "expo-application";
 import * as Sharing from "expo-sharing";
 import * as FileSystem from "expo-file-system";
 import * as DocumentPicker from "expo-document-picker";
+import * as Updates from "expo-updates";
 import { v4 as uuid } from "uuid";
 
 import * as db from "../../../storage/db";
 import * as PourStore from "../../../storage/PourStore";
 import { supabase } from "../../../storage/supabase";
+import { FontSize, Margin, TailwindColor } from "../../../constants/styles";
 
 export default function Settings() {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
+    <ScrollView
+      style={{ flex: 1, backgroundColor: TailwindColor.white }}
+      contentContainerStyle={{
+        minHeight: "100%",
         justifyContent: "center",
         backgroundColor: "white",
       }}
@@ -37,7 +40,13 @@ export default function Settings() {
       />
 
       <Button title="Upload images" onPress={() => uploadImagesAsync()} />
-      <Button title="Drop and create database" onPress={() => db.clear()} />
+      <Button
+        title="Drop and create database"
+        onPress={() => {
+          PourStore.destroyAll();
+          db.clear();
+        }}
+      />
       <Button
         title="Export data as JSON"
         onPress={() => exportDatabaseAsync()}
@@ -46,7 +55,51 @@ export default function Settings() {
         title="Import database from JSON"
         onPress={() => importDatabaseAsync()}
       />
-    </View>
+
+      <Text
+        style={{
+          fontWeight: "bold",
+          marginTop: Margin[5],
+          marginBottom: Margin[1],
+          color: TailwindColor["gray-700"],
+          fontSize: FontSize.lg,
+          textAlign: "center",
+        }}
+      >
+        Debug info
+      </Text>
+      <Text
+        style={{
+          marginTop: Margin[1],
+          fontSize: FontSize.base,
+          textAlign: "center",
+          color: TailwindColor["gray-700"],
+        }}
+      >
+        Version: {Application.nativeApplicationVersion} (
+        {Application.nativeBuildVersion})
+      </Text>
+      <Text
+        style={{
+          marginTop: Margin[1],
+          fontSize: FontSize.base,
+          textAlign: "center",
+          color: TailwindColor["gray-700"],
+        }}
+      >
+        ID: {Updates.updateId ?? "(no update id)"}
+      </Text>
+      <Text
+        style={{
+          fontSize: FontSize.base,
+          textAlign: "center",
+          color: TailwindColor["gray-700"],
+          marginTop: Margin[1],
+        }}
+      >
+        Released: {Updates.manifest.createdAt ?? "(no release date)"}
+      </Text>
+    </ScrollView>
   );
 }
 
@@ -93,7 +146,7 @@ async function uploadImagesAsync() {
         console.log(`uploading ${pour.photo_url}`);
         const url = await uploadImageAsync(pour.photo_url);
         console.log(`done uploading: ${url}`);
-        PourStore.update(pour.id, { ...pour, photo_url: url });
+        PourStore.updateAsync(pour.id, { ...pour, photo_url: url });
       }
     })
   );
