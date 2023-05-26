22698a54e934511ddc5a2b210e073586ad451280
Brent Vatne <brentvatne@gmail.com>
2023-05-26 11:09:02 -0700

    Set up Vexo

--- babel.config.js
+++ babel.config.js
@@ -6,6 +6,11 @@ module.exports = function (api) {
       require.resolve("react-native-reanimated/plugin"),
       // NOTE: `expo-router/babel` is a temporary extension to `babel-preset-expo`.
       require.resolve("expo-router/babel"),
+      ["transform-inline-environment-variables", {
+        "include": [
+          "VEXO_API_KEY"
+        ]
+      }],
       [require.resolve("babel-plugin-module-resolver"),
       {
         root: ["./"],
--- index.js
+++ index.js
@@ -7,4 +7,7 @@ import "react-native-get-random-values";
 // Register app entry through Expo Router
 import "expo-router/entry";
 
+import { vexo } from 'vexo-analytics'
+vexo(process.env.VEXO_API_KEY);
+
 LogBox.ignoreLogs(["Constants.platform.ios.model"]);
