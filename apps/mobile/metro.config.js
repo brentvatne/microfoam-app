ba18bc36c5fdce13fb8fef6aa9bbdd9974e040d9
Brent Vatne <brentvatne@gmail.com>
2023-12-19 16:23:52 -0800

    Bump and add patch

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,5 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-// const { createSentryMetroSerializer } = require('@sentry/react-native/metro');
+const { getSentryExpoConfig } = require('@sentry/react-native/metro');
 const path = require('path');
 
 // Find the project and workspace directories
@@ -7,7 +7,7 @@ const projectRoot = __dirname;
 // This can be replaced with `find-yarn-workspace-root`
 const workspaceRoot = path.resolve(projectRoot, '../..');
 
-const config = getDefaultConfig(projectRoot);
+const config = getSentryExpoConfig(__dirname);
 
 // 1. Watch all files within the monorepo
 config.watchFolders = [workspaceRoot];
@@ -18,7 +18,5 @@ config.resolver.nodeModulesPaths = [
 ];
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true;
-
-// config.serializer.customSerializer = createSentryMetroSerializer();
 
 module.exports = config;
