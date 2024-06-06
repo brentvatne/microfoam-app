aa336debb91f8b810d5abeab009be5928203a577
Douglas Lowder <douglowder@mac.com>
2024-06-05 19:58:23 -0700

    Add default channel

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -57,6 +57,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       {
         icon: "./assets/icon.png",
         color: "#232323",
+        defaultChannel: "default",
       },
     ],
     [
