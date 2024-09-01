30ac63272343229de2860f4d345ef6d57421acfd
Douglas Lowder <douglowder@mac.com>
2024-08-31 20:53:45 -0700

    Debug logging of prebuild

--- apps/mobile/scripts/prebuild-ios.sh
+++ apps/mobile/scripts/prebuild-ios.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-RELEASE=1 EX_UPDATES_NATIVE_DEBUG=1 npx expo prebuild --clean -p ios
+DEBUG=expo:* RELEASE=1 EX_UPDATES_NATIVE_DEBUG=1 npx expo prebuild --clean -p ios
+#DEBUG=expo:* RELEASE=1 npx expo prebuild --clean -p ios
