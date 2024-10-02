0c2b60db7ddae8c28e09c3dea60548d479dbeea5
Douglas Lowder <douglowder@mac.com>
2024-10-02 13:21:04 -0700

    Fix Android native debug script

--- apps/mobile/scripts/prebuild-android-nativedebug.sh
+++ apps/mobile/scripts/prebuild-android-nativedebug.sh
@@ -7,6 +7,6 @@
 #
 DEBUG=expo:* RELEASE=1 GOOGLE_SERVICES_JSON=~/google-services-microfoam.json npx expo prebuild --clean -p android
 
-echo 'EX_UPDATES_NATIVE_DEBUG=1' >> android/gradle.properties
+sed -i '' 's/android.extraMavenRepos.*$/EXPO_UPDATES_NATIVE_DEBUG=1/;' android/gradle.properties
 
 sed -i '' 's/\/\/ debuggableVariants = .*$/  debuggableVariants = []/;' android/app/build.gradle
